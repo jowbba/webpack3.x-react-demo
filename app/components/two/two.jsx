3d9fc05447a402b4023b2d1258cf5f2ababe4bdf
harry <411981379@qq.com>
2017-11-13 18:11:25 +0800

    seperate config file

--- app/components/two/two.jsx
+++ app/components/two/two.jsx
@@ -11,7 +11,7 @@ class Two extends React.Component {
 	render() {
 		return (
 			<div>
-				<h2 className='title'>this is test component {this.state.number}</h2>
+				<h2 className='title'>1this is test component {this.state.number}</h2>
 				<img src={testImage} alt="" width='600'/>
 			</div>
 		)
